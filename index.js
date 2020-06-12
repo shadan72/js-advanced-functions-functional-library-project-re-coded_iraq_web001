8e30a7e83a8e08dcc71425b5c4a55e0d95ef61ad
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-12 13:32:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-  
+    each: function(arr, callback) {
+ 
+      for(const ele in arr){
+        callback(arr[ele], ele, arr)
+      }
+    }
     },
 
     map: function(arr, callback) {
